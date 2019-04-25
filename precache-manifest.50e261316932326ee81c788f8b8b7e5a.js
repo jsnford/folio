e786fec9bcfb396db8be6c31d67621387ca2b661
Jason Ford <jsn.ford@gmail.com>
2019-04-25 16:37:19 +0100

    Updates

---
+++ precache-manifest.50e261316932326ee81c788f8b8b7e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45b3190042ed77b6bd1501202ee164b",
+    "url": "/folio/index.html"
+  },
+  {
+    "revision": "ed1411f05a01fc55afe5",
+    "url": "/folio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/folio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ed1411f05a01fc55afe5",
+    "url": "/folio/static/js/main.d5e802e0.chunk.js"
+  },
+  {
+    "revision": "ee062638373429dce42b",
+    "url": "/folio/static/js/runtime~main.0742296d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/folio/static/media/logo.5d5d9eef.svg"
+  }
+]);
